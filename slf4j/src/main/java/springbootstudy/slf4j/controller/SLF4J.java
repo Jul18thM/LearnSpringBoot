91da9314c44e16ad330af8b3b28a48a3e46e3886
Jul18thM <jul.18th.m@gmail.com>
2019-01-22 21:29:29 +0800

    SpringBoot学习之旅（三）：日志

---
+++ slf4j/src/main/java/springbootstudy/slf4j/controller/SLF4J.java
@@ -0,0 +1,15 @@
+package springbootstudy.slf4j.controller;
+
+/**
+ * @ClassName SLF4J日志
+ * @Description TODO
+ * @Author mmw
+ * @Date 2019/1/21 19:56
+ * @Version 1.0
+ **/
+
+
+public class SLF4J {
+
+
+}
